238b3a9b8290538cd2edd312fff4b42f3d2535e2
Gustavo Acácio <gustavo.acacio@attus.ai>
2024-07-17 22:30:15 -0300

    Criacao de testes

--- src/main/java/com/gustavoacacio/listadecompra/exception/ListaExcepion.java
+++ src/main/java/com/gustavoacacio/listadecompra/exception/ListaExcepion.java
@@ -3,11 +3,11 @@ package com.gustavoacacio.listadecompra.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 
-public class ListaExcepion extends RuntimeException{
+public class ListaExcepion extends RuntimeException {
 
-    public ProblemDetail toProblemDetail(){
+    public ProblemDetail toProblemDetail() {
         var pb = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
         pb.setTitle("Erro interno");
-        return  pb;
+        return pb;
     }
 }
