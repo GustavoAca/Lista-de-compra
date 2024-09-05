48ceeb4a4dd6e0e4491a1ea255925f0d8a0d97fa
Gustavo Acácio <gustavo.acacio@attus.ai>
2024-09-05 18:34:50 -0300

    ajustes de mensagem de erro

--- src/main/java/com/gustavoacacio/listadecompra/controller/RestExceptionHandler.java
+++ src/main/java/com/gustavoacacio/listadecompra/controller/RestExceptionHandler.java
@@ -4,6 +4,7 @@ import com.gustavoacacio.listadecompra.exception.ListaExcepion;
 import org.hibernate.PersistentObjectException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -36,5 +37,13 @@ public class RestExceptionHandler {
     }
 
     private record InvalidParam(String fieldName, String reason) {
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ProblemDetail handlerCannotDeserialize(HttpMessageNotReadableException e) {
+        var pb = ProblemDetail.forStatus(HttpStatus.UNPROCESSABLE_ENTITY);
+        pb.setTitle("Erro ao deserializar UUID");
+        pb.setDetail(e.getMessage());
+        return pb;
     }
 }
