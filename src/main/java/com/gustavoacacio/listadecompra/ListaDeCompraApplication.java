d35120a11632ca5800de851b7e87e01b6638e339
Gustavo Acácio <gustavo.acacio@attus.ai>
2024-10-07 17:55:53 -0300

    remocaco rabbitMQ

--- src/main/java/com/gustavoacacio/listadecompra/ListaDeCompraApplication.java
+++ src/main/java/com/gustavoacacio/listadecompra/ListaDeCompraApplication.java
@@ -3,11 +3,12 @@ package com.gustavoacacio.listadecompra;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
-//@ComponentScan(value = "com.gustavoacacio.listadecompra.domain.model")
+@ComponentScan(value = "com.gustavoacacio.listadecompra.domain.model")
 public class ListaDeCompraApplication {
 
     public static void main(String[] args) {
